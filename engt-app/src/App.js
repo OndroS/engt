409ed13918de84df41e84e0738fa553b7ed951b5
OndroS <hello@ondrejsarnecky.com>
2023-07-25 22:44:48 +0200

    Answer check implemented

--- engt-app/src/App.js
+++ engt-app/src/App.js
@@ -25,7 +25,7 @@ const Question = ({ question, answers, onSelectAnswer }) => (
     <div className='Question'>
         <div>
             <h1 className='Question-text'>{question.text}</h1>
-            <div className='Answers'>
+            {/* <div className='Answers'>
                 {answers.map((answer) => (
                     <div key={answer._id}>
                         <button
@@ -37,7 +37,7 @@ const Question = ({ question, answers, onSelectAnswer }) => (
                         <br/>
                     </div>
                 ))}
-            </div>
+            </div> */}
         </div>
     </div>
 );
@@ -46,55 +46,78 @@ function App() {
     const { loading, error, data } = useQuery(GET_QUESTIONS);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState({});
-    // const [correctOrFalse, setCorrectOrFalse] = useState(false)
-
-    console.log('data ', data)
-
+  
     if (error) return <h1>Something went wrong!</h1>;
     if (loading) return <h1>Loading...</h1>;
-
+  
     const handleSelectAnswer = (questionId, answerId, correct) => {
-        setUserAnswers((prevUserAnswers) => ({
-            ...prevUserAnswers,
-            [questionId]: {
-                answerId,
-                correct
-            },
-        }));
+      setUserAnswers((prevUserAnswers) => ({
+        ...prevUserAnswers,
+        [questionId]: {
+          answerId,
+          correct,
+        },
+      }));
     };
-
+  
     const handleNextQuestion = () => {
-        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
     };
-
+  
     const handleFinish = () => {
-        console.log('User Answers:', userAnswers);
+      console.log('User Answers:', userAnswers);
     };
-
+  
     const currentQuestion = data.getAllQuestionsWithAnswers[currentQuestionIndex].question;
     const currentAnswers = data.getAllQuestionsWithAnswers[currentQuestionIndex].answers;
     const isLastQuestion = currentQuestionIndex === data.getAllQuestionsWithAnswers.length - 1;
-
+  
     return (
-        <main className='App'>
-            <h1>Python: Podmínky a metody</h1>
-            <p>Lekce {currentQuestionIndex + 1} / {data.getAllQuestionsWithAnswers.length}: Podmínky</p>
-            <Question
-                question={currentQuestion}
-                answers={currentAnswers}
-                onSelectAnswer={handleSelectAnswer}
-            />
-            {!isLastQuestion ? (
-                <button onClick={handleNextQuestion} className='Next-btn'>
-                    Next
-                </button>
-            ) : (
-                <button onClick={handleFinish} className='Finish-btn'>
-                    Finish
-                </button>
-            )}
-        </main>
+      <main className='App'>
+        <h1>Python: Podmínky a metody</h1>
+        <p>
+          Lekce {currentQuestionIndex + 1} / {data.getAllQuestionsWithAnswers.length}: Podmínky
+        </p>
+        <Question question={currentQuestion} answers={currentAnswers} onSelectAnswer={handleSelectAnswer} />
+        {currentAnswers.map((answer) => {
+          let buttonClassName = 'Answer-btn';
+          let buttonText = answer.text;
+          if (userAnswers[currentQuestion._id]) {
+            if (answer._id === userAnswers[currentQuestion._id].answerId) {
+              if (answer.correct) {
+                buttonClassName = 'Correct-answer-btn';
+                buttonText = 'Correct answer!';
+              } else {
+                buttonClassName = 'Wrong-answer-btn';
+                buttonText = 'Wrong answer';
+              }
+            } else if (answer.correct) {
+              buttonClassName = 'Correct-answer-btn';
+              buttonText = 'This is the correct answer';
+            }
+          }
+  
+          return (
+            <div key={answer._id}>
+              <button onClick={() => handleSelectAnswer(currentQuestion._id, answer._id, answer.correct)} className={buttonClassName}>
+                {buttonText}
+              </button>
+              <br />
+            </div>
+          );
+        })}
+
+        {!isLastQuestion ? (
+          <button onClick={handleNextQuestion} className='Next-btn'>
+            Next
+          </button>
+        ) : (
+          <button onClick={handleFinish} className='Finish-btn'>
+            Finish
+          </button>
+        )}
+      </main>
     );
-}
+  }
 
 export default App;
